c4f04e5fff968d3a68b008ed08010d83f98d1729
mo3badah <20812016100828@eng.zu.edu.eg>
2023-07-01 02:27:47 +0300

    sequelize connect to aws rds.

--- models/sequelize.js
+++ models/sequelize.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('GraduationProject', 'root', 'Password123#@!', {
-    host: 'localhost',
+const sequelize = new Sequelize('GraduationProject', 'admin', 'mo3badah2023', {
+    host: 'my-nodejs-database-instance-1.c1dsu1fi8pdh.us-east-1.rds.amazonaws.com',
     dialect: 'mysql'
 });
 
